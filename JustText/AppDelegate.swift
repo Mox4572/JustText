a112ef1143d51d5ce1781d6c31e718163052af95
Teddy Gaillard <teddy.gaillard@icloud.com>
2022-01-05 20:02:01 -0800

    Add "Open Markdown Editor" functionality

--- JustText/AppDelegate.swift
+++ JustText/AppDelegate.swift
@@ -9,22 +9,30 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
+	@IBOutlet var window: NSWindow!
+	
+    func applicationDidFinishLaunching(_ aNotification: Notification) { }
+	
+    func applicationWillTerminate(_ aNotification: Notification) { }
 
-    @IBOutlet var window: NSWindow!
-
-
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
-    }
-
-
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool { return true }
+	
+	@IBAction func openMarkdownTool(_ sender: NSMenuItem) {
+		if FileManager.default.fileExists(atPath: "/Applications/Markdown Tool.app") {
+			let url = NSURL(fileURLWithPath: "/Applications/Markdown Tool.app", isDirectory: true) as URL
+			NSWorkspace.shared.openApplication(at: url, configuration: NSWorkspace.OpenConfiguration())
+		} else {
+			let alert = NSAlert()
+			alert.alertStyle = .warning
+			alert.messageText = "You don't have Markdown Tool installed"
+			let okButton = alert.addButton(withTitle: "OK")
+			let moreInformationButton = alert.addButton(withTitle: "More Information")
+			okButton.tag = NSApplication.ModalResponse.OK.rawValue
+			moreInformationButton.tag = NSApplication.ModalResponse.continue.rawValue
+			let response = alert.runModal()
+			if response == .continue {
+				NSWorkspace.shared.open(URL(string: "https://github.com/Squid4572/MarkdownTool/releases")!)
+			}
+		}
+	}
 }
-
