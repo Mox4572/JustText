684c8f96a3218480968cd000ea9dbc82c2a1fa20
Astro Kid <71262643+MDNich@users.noreply.github.com>
2022-02-22 21:10:17 -0500

    create new target, not ready yet. Will include iOS port later

---
+++ JustTextNew/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  JustTextNew
+//
+//  Created by Marc D. Nichitiu on 2/22/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ JustTextNew/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  JustTextNew
+//
+//  Created by Marc D. Nichitiu on 2/22/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
